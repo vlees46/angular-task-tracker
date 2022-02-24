fac64ce822a4819dbcdb21514fe79cf4d1661235
Victor Lees <vlees14.pa@gmail.com>
2022-02-24 14:13:55 -0500

    task.components.ts .deleteTask() was missing the Task variable, corrected

--- task-tracker/src/app/components/tasks/tasks.component.ts
+++ task-tracker/src/app/components/tasks/tasks.component.ts
@@ -18,5 +18,11 @@ export class TasksComponent implements OnInit {
 
 
   }
+  deleteTask(task: Task){
+    this.taskService
+    .deleteTask(task)
+    .subscribe(
+      () => (this.tasks = this.tasks.filter(t => t.id! !== task.id)));
+  }
 
 }
--- task-tracker/src/app/services/task.service.ts
+++ task-tracker/src/app/services/task.service.ts
@@ -9,11 +9,17 @@ import { Task } from '../Task';
 })
 export class TaskService {
 
-  private apiURL = 'http://localhost:5000/tasks'
+  private apiUrl = 'http://localhost:5000/tasks'
 
   constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(this.apiURL)
+    return this.http.get<Task[]>(this.apiUrl);
   }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+
 }
