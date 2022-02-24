10b3e156cb4585c0d0b28b16d4c3d6fc985f0d44
Victor Lees <vlees14.pa@gmail.com>
2022-02-24 13:13:15 -0500

    installed HttpClient and HttpHeader to perform GET operation on JSON Server on locally served PC

--- task-tracker/src/app/services/task.service.ts
+++ task-tracker/src/app/services/task.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable, of} from 'rxjs'
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private apiURL = 'http://localhost:5000/tasks'
+
+  constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiURL)
   }
 }
