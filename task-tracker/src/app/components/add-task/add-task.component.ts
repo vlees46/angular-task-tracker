50424b71fc1a06da7ee9c34aae0652a7177a022b
Victor Lees <vlees14.pa@gmail.com>
2022-02-24 18:25:12 -0500

    final commit added about page and routing

--- task-tracker/src/app/components/add-task/add-task.component.ts
+++ task-tracker/src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {Task} from '../../Task';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -6,11 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
+
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
   text!: string;
   day!: string;
   reminder: boolean = false;
+  showAddTask!: boolean;
+  subscription!: Subscription;
 
-  constructor() { }
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService
+    .onToggle()
+    .subscribe((value) => (this.showAddTask = value));
+
+  }
 
   ngOnInit(): void {
   }
@@ -21,7 +33,13 @@ export class AddTaskComponent implements OnInit {
     }
     const newTask = {
       text: this.text,
-      day: this.day
+      day: this.day,
+      reminder: this.reminder
     }
+    this.onAddTask.emit(newTask);
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
   }
+
 }
--- task-tracker/src/app/services/task.service.ts
+++ task-tracker/src/app/services/task.service.ts
@@ -32,4 +32,9 @@ export class TaskService {
     return this.http.put<Task>(url, task, httpOptions);
   }
 
+  addTask(task: Task): Observable<Task>{
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+
+  }
+
 }
