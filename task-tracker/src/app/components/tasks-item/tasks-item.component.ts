7eaf6be76353e6016fcb1112dee8a5f884345a2f
Victor Lees <vlees14.pa@gmail.com>
2022-02-24 12:12:40 -0500

    added angular font awesome

---
+++ task-tracker/src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {Task} from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent implements OnInit {
+  @Input() task!: Task
+  faTimes = faTimes;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
