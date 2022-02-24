964296bf8a872e4ebe36d99bcf482383ec6d42f7
Victor Lees <vlees14.pa@gmail.com>
2022-02-24 12:50:29 -0500

    created a observable for getting the array of tasks

--- task-tracker/src/app/components/tasks/tasks.component.ts
+++ task-tracker/src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {TASKS} from '../../mock-tasks';
-import {Task} from '../../Task';
+import { TaskService } from '../../services/task.service';
+
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
@@ -8,10 +9,14 @@ import {Task} from '../../Task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
-  constructor() { }
+  tasks: Task[] = [];
+  constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => 
+    (this.tasks = tasks));
+
+
   }
 
 }
