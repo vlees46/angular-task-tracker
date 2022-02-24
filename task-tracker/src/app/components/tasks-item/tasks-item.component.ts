6a3578ac416531402da5f15fc36c2095345cdccf
Victor Lees <vlees14.pa@gmail.com>
2022-02-24 14:50:37 -0500

    created two methods deleteTask and toggleReminder using observables to communicate wit the JSON server

--- task-tracker/src/app/components/tasks-item/tasks-item.component.ts
+++ task-tracker/src/app/components/tasks-item/tasks-item.component.ts
@@ -10,6 +10,7 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 export class TasksItemComponent implements OnInit {
   @Input() task!: Task
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter()
   faTimes = faTimes;
 
   constructor() {}
@@ -18,6 +19,10 @@ export class TasksItemComponent implements OnInit {
 
   onDelete(task){
     this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task){
+    this.onToggleReminder.emit(task);
   }
 
 }
--- task-tracker/src/app/components/tasks/tasks.component.ts
+++ task-tracker/src/app/components/tasks/tasks.component.ts
@@ -24,5 +24,8 @@ export class TasksComponent implements OnInit {
     .subscribe(
       () => (this.tasks = this.tasks.filter(t => t.id! !== task.id)));
   }
-
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
 }
